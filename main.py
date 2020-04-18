d077e815a21bb9885263e22e89eabec9af52dbf1
Shadowlucky <46817269+Shadowlucky@users.noreply.github.com>
2020-04-18 15:17:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ run_with_ngrok(app)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask"
+    return "Привет от приложения Flask!"
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(host='127.0.0.1')
     
