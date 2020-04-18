1748abdcd1eb033ba65d4b18c3721566b4135ee0
Egor <egor-lavr.ru@mail.ru>
2020-04-18 16:15:04 +0500

    Test

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ run_with_ngrok(app)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask"
+    return f"""<h1 align="center">Приветствуем на главной странице</h1>
+               <a href="/game">Игра</a>"""
+
+
+@app.route("/game")
+def game():
+    return f"""<h1 align="center">PanzerStorm</h1>
+                <a href="/">На главную страницу</a>
+                """
 
 
 if __name__ == '__main__':
