8343076fafefde41ab284b896e0866b839369351
Shadowlucky <46817269+Shadowlucky@users.noreply.github.com>
2020-04-18 11:42:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
 
 @app.route("/")
 def index():
-    return "Привет, мир"
+    return "Привет от приложения Flask"
 
 
 if __name__ == '__main__':
